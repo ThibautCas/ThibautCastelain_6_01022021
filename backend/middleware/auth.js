1d29892d2ed25c7621b081da4032e5a1ef9a042b
Thibaut Cas <thibautcastelain@gmail.com>
2021-02-08 18:02:52 +0100

    replacing auth token by env variable

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
+const AUTH_TOKEN = process.env.AUTH_TOKEN
 
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, AUTH_TOKEN);
         const userId = decodedToken.userId;
         if (req.body.userId && req.body.userId !== userId) {
             throw 'User ID non valable !';
