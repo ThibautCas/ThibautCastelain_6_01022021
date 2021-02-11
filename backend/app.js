7d99ca1c039d40642bd47eb8597664bdbacae8ed
Thibaut Cas <thibautcastelain@gmail.com>
2021-02-11 15:48:02 +0100

    updated security with helmet and sanitize

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
-require('dotenv').config();
+require('dotenv').config();                 //For environement variables (not to show passwords in public source code)
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 const mongodbURL = process.env.DB_URL;
+
 mongoose.connect(mongodbURL,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -15,6 +18,7 @@ mongoose.connect(mongodbURL,
 
 const app = express();
 
+app.use(helmet());                        //For securized HTTP headers (against XSS attacks)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -22,7 +26,7 @@ app.use((req, res, next) => {
     next();
   });
 app.use(bodyParser.json());
-
+app.use(mongoSanitize());                 //For sanitization against NoSQL query injection
 app.use('/images', express.static(path.join(__dirname, 'images')));
 app.use('/api/sauces', sauceRoutes); 
 app.use('/api/auth', userRoutes); 
