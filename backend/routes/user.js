9572c54fd7d186fbf19b8658cce3cbcff1887700
Thibaut Cas <thibautcastelain@gmail.com>
2021-02-13 10:20:56 +0100

    implemented bouncer against brute-force attacks

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const bouncer = require ("express-bouncer")(500, 900000); // Against brute-force attacks
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', bouncer.block, userCtrl.login);
 
 module.exports = router;
